f97f509ca2f57cbc59e55ab97f12c72960bc2a6d
rkes <rakesh.nit.mca@gmail.com>
2017-06-18 22:36:07 +0530

    First React Application With Youtube API

---
+++ src/components/video_list_item.js
@@ -0,0 +1,29 @@
+import React from 'react';
+const VideoListItem =(props)=>{
+    const video=props.video;
+    const imgUrl=video.snippet.thumbnails.default.url;
+
+    const onVideoSelect=props.onVideoSelect;
+     return(
+        <li onClick={
+                    ()=>
+                        {
+                            //console.log(video);
+                            onVideoSelect(video);
+                            console.log(onVideoSelect);
+                        }
+                    } className="list-group-item">
+            <div className="video-list-media">
+                <div className="media-left">
+                    <img className="media-object" src={imgUrl}/>
+                </div>
+
+                <div className="media-body">
+                    <div className="media-heading">{video.snippet.title}</div>
+                </div>
+            </div>
+        </li>
+    );
+};
+
+export default VideoListItem;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,51 @@
-import React from 'react';
+// Create a new Component Should Produce some HTML
+//Take this component's Genertaed HTML and put in Page i.e. in DOM
+import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
+import VideoDetails from './components/video_detail';
+import _ from 'lodash';
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY='AIzaSyC0YtYNTnWRAsO2k0yFgTpKOVfs4VMpSpk';
+class App extends Component{
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+    constructor(props){
+        super(props);
+        this.state={
+                videos:[],
+                selectedVideo:null
+        };
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+        this.videoSearch("pune");
+    }
+    videoSearch(term){
+        YTSearch({key:API_KEY,term:term},(videos)=> {
+            this.setState({
+                videos:videos,
+                selectedVideo:videos[0]
+            });
+        });
+    }
+    render()
+    {
+        const videoSearch=_.debounce((term)=>{this.videoSearch(term)},300);
+        return (
+            <div>
+                <SearchBar
+                    onSearchTermChange={videoSearch}
+                />
+                <VideoDetails
+                    video={this.state.selectedVideo}
+                />
+                <VideoList
+                    onVideoSelect={selectedVideo=>this.setState({selectedVideo})}
+                    videos={this.state.videos}
+                />
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App/>,document.querySelector('.container'));
